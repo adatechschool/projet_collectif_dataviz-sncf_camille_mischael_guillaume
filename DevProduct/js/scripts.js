df3225fe7c439cb65e63d062a1fe6981b3c6e3db
MischaelP <phemius.p@gmail.com>
2023-03-28 22:31:29 +0200

    [FEATURE] Ajout des affichages en JS sur toutes les pages

--- DevProduct/js/scripts.js
+++ DevProduct/js/scripts.js
@@ -24,16 +24,15 @@
         
 //     }
 // }
-// getUrl()
 
-console.log(data[4].piano)
+let indice = findGareIndice()
+
 
 function piano() {
     let container = document.querySelector(".b")
     let image = document.createElement("p");
     let string = document.createElement("p");
-    let piano = data[4].piano
-    console.log(typeof(piano))
+    let piano = data[indice].piano
 
     if (piano == true) {
         image.innerHTML = `<img src="img/piano-icon.svg" />`;
@@ -55,12 +54,13 @@ function panier_fraicheur() {
     let container = document.querySelector(".a")
     let image = document.createElement("p");
     let string = document.createElement("p");
-    let panier = data[4].paniers
-    console.log(typeof(paniers))
+    let panier = data[indice].paniers
+    
+
     
     if (panier == true) {
         image.innerHTML = `<img src="img/panier-icon.svg" />`;
-        string.innerHTML = `Vous pouvez récupérer votre panier fraîcheur le ${data[4].jour} de ${data[4].horaires}`;
+        string.innerHTML = `Vous pouvez récupérer votre panier fraîcheur le ${data[indice].jour} de ${data[indice].horaires}`;
         container.appendChild(image);
         container.appendChild(string);
         
@@ -77,8 +77,8 @@ function wifi() {
     let container = document.querySelector(".d")
     let image = document.createElement("p");
     let string = document.createElement("p");
-    let wifi = data[4].Wifi
-    console.log(typeof(wifi))
+    let wifi = data[indice].Wifi
+    
     
     if (wifi == true) {
         image.innerHTML = `<img src="img/wifi-icon.svg" />`;
@@ -99,8 +99,8 @@ function borne() {
     let container = document.querySelector(".c")
     let image = document.createElement("p");
     let string = document.createElement("p");
-    let borne = data[4].bornes
-    console.log(typeof(borne))
+    let borne = data[indice].bornes
+    
     
     image.innerHTML = `<img src="img/sos-icon.svg" />`;
     string.innerHTML = `Vous avez ${borne} bornes d'appel disponibles dans votre gare.`;
@@ -118,4 +118,3 @@ wifi()
 borne()
 
 let title = document.querySelector("title").textContent
-console.log(title)
--- DevProduct/js/test.js
+++ DevProduct/js/test.js
@@ -6,7 +6,18 @@ function findGare(){
             return data[i]
         }
     }
+console.table(findGare())
+
+}
+
+function findGareIndice(){
+    let title = document.querySelector("title").textContent
+    
+    for (let i = 0;i< data.length; i++){
+        if(data[i].gare == title){
+            return i
+        }
+    }
 
 
 }
-console.log(findGare())
